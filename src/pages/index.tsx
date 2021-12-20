1bcff0e3e58dd6f95214779a1f91bb8b6ff85780
glaubermatos <glaub.oliveira@hotmail.com>
2021-12-20 16:37:57 -0300

    home page menu and perfil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import commomStyles from '../styles/commom.module.scss'
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
-    <h1>JobsCalc</h1>
+    <>
+      <header className={styles.header}>
+        <div className={commomStyles.container}>
+          <div className={styles.menu}>
+            <img src="/logo.svg" alt="logo" />
+            <span>
+              <img src="/alert-octagon.svg" alt="alert" />
+              Você tem 2 horas livres no seu dia
+            </span>
+            <div className={styles.perfil}>
+              <div>
+                <strong>Jaqueline</strong>
+                <span>Ver perfil</span>
+              </div>
+              <img src="/perfil.png" alt="perfil" />
+            </div>
+          </div>
+          <div className={styles.summary}>
+             
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
